411f5bff9552cfd2a1927233d047ebbaa8598e6d
Wolfger Schramm <wolfger@spearwolf.de>
2020-08-15 12:56:06 +0000

    improve setup and build scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-source ./setup-go-env.sh
+if [ -z $(which go) ]
+then
+    echo "couldn't find the go executable in your PATH :("
+    echo "please install the go sdk first"
+    echo "run 'source ./setup-go-env.sh' if you have a linux terminal"
+    echo "or visit https://golang.org/ for detailed instructions"
+    exit 1
+fi
 
-go build -o kiwotigo kiwotigo-tool/main.go
+CMD="go build -o kiwotigo kiwotigo-tool/main.go"
+echo $CMD
+exec $CMD
--- setup-go-env.sh
+++ setup-go-env.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# USE WITH $ source setup-go-env.sh
-
 GO_ARCHIVE=go1.15.linux-amd64.tar.gz
 
 if [ -z $(which go) ]
@@ -17,6 +15,12 @@ then
         sudo tar -C /usr/local -xzf $GO_ARCHIVE
     fi
     export PATH=$PATH:/usr/local/go/bin
-    echo "added /usr/local/go/bin to your PATH environment"
-    go version
+
+    echo "added /usr/local/go/bin to your PATH env"
+    echo "if you want to set the PATH permanently, add the following line to your $HOME/.profile"
+    echo "export PATH=$PATH:/usr/local/go/bin"
 fi
+echo -n "Great! you have installed "
+go version
+
+echo "Go and build the kiwotigo tool with ./build.sh"
