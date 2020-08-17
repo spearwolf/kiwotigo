80ad5c9886b62525292b0f19507e3a6f7e9df8c8
Wolfger Schramm <wolfger@spearwolf.de>
2020-08-17 10:41:53 +0200

    introduce kiwotigo.mjs

---
+++ kiwotigo.mjs
@@ -0,0 +1,44 @@
+import "./wasm_exec.js";
+
+const go = new Go();
+const __kiwotiGo = WebAssembly.instantiateStreaming(
+  fetch("kiwotigo.wasm"),
+  go.importObject
+).then((result) => {
+  go.run(result.instance);
+});
+
+export const DefaultConfig = {
+  gridWidth: 10,
+  gridHeight: 10,
+  gridOuterPaddingX: 25,
+  gridOuterPaddingY: 25,
+  gridInnerPaddingX: 6,
+  gridInnerPaddingY: 3,
+  gridHexWidth: 16,
+  gridHexHeight: 14,
+  hexWidth: 12, //24,
+  hexHeight: 12,
+  hexPaddingX: 0, //5,  //3,
+  hexPaddingY: 0, //5,  //3,
+  fastGrowIterations: 8, //10,
+  minimalGrowIterations: 120, //48,
+  maxRegionSizeFactor: 3,
+  divisibilityBy: 1,
+  probabilityCreateRegionAt: 0.6,
+};
+
+export function createContinent(cfg) {
+  return __kiwotiGo.then(
+    () =>
+      new Promise((resolve) => {
+        __kiwotiGo_createContinent(
+          {
+            DefaultConfig,
+            ...cfg,
+          },
+          (result) => resolve(JSON.parse(result))
+        );
+      })
+  );
+}
