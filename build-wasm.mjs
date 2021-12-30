97202bfc57b75a63a85f2a837adcf60119256346
Wolfger Schramm <wolfger@spearwolf.de>
2021-12-30 10:51:00 +0100

    refactor build scripts DRY

--- build-wasm.mjs
+++ build-wasm.mjs
@@ -2,37 +2,18 @@
 
 import fs from 'fs';
 import path from 'path';
-import { platform } from 'process';
 import 'zx/globals';
-
-const GOEXE = platform.startsWith('win') ? 'go.exe' : 'go';
-
-const readGoRoot = async () => {
-  try {
-    return (await $`${GOEXE} env GOROOT`).toString().trim();
-  } catch (err) {
-    console.error(`couldn't find the _go_ executable in your PATH :(
-please install the go sdk first
-run 'source ./setup-go-env.sh' if you have a linux terminal
-or visit https://golang.org/ for detailed instructions
-`);
-    process.exit(1);
-  }
-};
+import initGoEnv from './scripts/initialize-go-env.mjs';
 
 const syncWasmExecScript = (goRoot, target) => {
-  const data = fs.readFileSync(
-    path.join(goRoot, 'misc/wasm/wasm_exec.js'),
-    'utf8',
-  );
+  const data = fs.readFileSync(path.join(goRoot, 'misc/wasm/wasm_exec.js'), 'utf8');
   fs.writeFileSync(target, data, 'utf8');
 };
 
-const goRootDir = await readGoRoot();
+const {GOEXE, GOROOT} = await initGoEnv();
 
-syncWasmExecScript(goRootDir, 'wasm_exec.js');
+syncWasmExecScript(GOROOT, 'wasm_exec.js');
 
-process.env.GO111MODULE = 'off';
 process.env.GOOS = 'js';
 process.env.GOARCH = 'wasm';
 
