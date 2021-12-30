0a4943445dc9c5bbb3ee83e4ffbabbe109f6121d
Wolfger Schramm <wolfger@spearwolf.de>
2021-12-30 11:24:05 +0100

    add more info to build script output

--- build-wasm.mjs
+++ build-wasm.mjs
@@ -7,7 +7,13 @@ import initGoEnv from './scripts/initialize-go-env.mjs';
 
 const syncWasmExecScript = (goRoot, target) => {
   const data = fs.readFileSync(path.join(goRoot, 'misc/wasm/wasm_exec.js'), 'utf8');
-  fs.writeFileSync(target, data, 'utf8');
+  const oldData = fs.existsSync(target) && fs.readFileSync(target, 'utf8');
+  if (data !== oldData) {
+    console.log('write', target);
+    fs.writeFileSync(target, data, 'utf8');
+  } else {
+    console.log('leave', target, 'untouched - it is up to date');
+  }
 };
 
 const {GOEXE, GOROOT} = await initGoEnv();
